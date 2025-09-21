8221c0632aadf799bd056032171987172880c49d
Alejandro <yasdmi048@gmail.com>
2025-09-21 13:43:44 -0500

    feat :art: midific in code

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,36 @@
-const envoltura=document.querySelector(".envoltura");
-const carta =document.querySelector(".cartaFull");
-document.addEventListener("click",(e)=>{
-    if (e.target.matches(".sobre")||
-    e.target.matches(".izquierda")||e.target.matches(".derecha")|| 
-    e.target.matches(".corazon")){
-            envoltura.classList.toggle("abierto");
+const envoltura = document.querySelector(".envoltura");
+const carta = document.querySelector(".cartaFull");
+const sobre = document.querySelector(".sobre");
 
+document.addEventListener("click",(e)=>{
+    if (e.target.matches(".sobre") ||
+        e.target.matches(".izquierda") ||
+        e.target.matches(".derecha") || 
+        e.target.matches(".corazon")) {
+            
+        envoltura.classList.toggle("abierto");
+        sobre.classList.add("desabilitar"); 
+
+        if (!carta.classList.contains("abierta")) {
+            setTimeout(() => {
+                carta.classList.add("mostarCarta");
+                setTimeout(()=>{
+                    carta.classList.remove("mostarCarta");
+                    carta.classList.add("abierta");
+                },500);
+            },1000);
+        }
     }
-    if (!carta.classList.contains("mostarCarta")) {
-        setTimeout(() => {
-            carta.classList.add("mostarCarta");
-        }, 1000);
+
+    if (!carta.contains(e.target) && !envoltura.contains(e.target)) {
+        envoltura.classList.remove("abierto");
+        if (carta.classList.contains("abierta")) {
+            carta.classList.add("cerrar");
+            setTimeout(() => {
+                carta.classList.remove("cerrar");
+                carta.classList.remove("abierta");
+                sobre.classList.remove("desabilitar"); 
+            }, 600);
+        }
     }
 });
